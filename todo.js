6670948e7f2648fd48563de57c902347b2db3d25
Arun <prasathsid2000@gmail.com>
2024-08-30 13:37:17 +0530

    Add files via upload
    new uploaded

---
+++ todo.js
@@ -0,0 +1,13 @@
+var fruit = ["Apple","Orange","Pinaple","Graps","Gova"]
+ 
+console.log(fruit.length)
+
+
+for(count=0;count<fruit.length;count=count+1)
+    {
+        console.log(fruit[(count)])
+    }
+
+
+
+
